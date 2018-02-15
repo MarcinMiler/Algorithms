b62a8a046a999b1984303eb363ea0d822121b10e
Marcin <marcin4951@gmail.com>
2018-02-15 20:46:00 +0100

    standard quick sort

--- index.js
+++ index.js
@@ -84,3 +84,30 @@ const swap = (x, i, j) => {
     x[i] = x[j]
     x[j] = temp
 }
+
+export const standardQuickSort = x => {
+    standardQuick(x, 0, x.length - 1)
+    return x
+}
+
+const standardQuick = (x, left, right) => {
+    if(left >= right) return
+
+    let pivot = x[right]
+    let border = left - 1
+    let i = left
+
+    while(i < right) {
+        if(x[i] < pivot) {
+            border++
+            if(border != i) swap(x, border, i)
+            
+        }
+        i++
+    }
+    border++
+    if(border != right) swap(x, border, right)
+
+    standardQuick(x, left, border - 1)
+    standardQuick(x, border + 1, right)
+}
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import { insertionSort, mergeSort, heapSort } from './index'
+import { insertionSort, mergeSort, heapSort, standardQuickSort } from './index'
 
 const noSorted = [33, 8, 63, 41, 72, 91, 47, 7, 90, 10, 25, 13, 30, 7, 62, 74, 92, 4, 22, 98, 76, 3, 9, 51, 86, 22, 99, 41, 40, 66, 19, 50, 51, 5, 73, 44, 81, 30, 61]
 const sorted = [3, 4, 5, 7, 7, 8, 9, 10, 13, 19, 22, 22, 25, 30, 30, 33, 40, 41, 41, 44, 47, 50, 51, 51, 61, 62, 63, 66, 72, 73, 74, 76, 81, 86, 90, 91, 92, 98, 99]
@@ -15,5 +15,10 @@ test('merge sort', () => {
 
 test('heap sort', () => {
     expect(heapSort(noSorted))
+    .toEqual(sorted)
+})
+
+test('quick sort', () => {
+    expect(standardQuickSort(noSorted))
     .toEqual(sorted)
 })
